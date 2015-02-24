d4ba2c951209fa25f0ca4936e2d3b9fe0a90ac13
nikmolnar <nik.molnar@consbio.org>
2015-02-23 20:20:12 -0800

    Correctly install wsdl xml files

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     name='python-josso-auth',
     description='A JOSSO backend for python-social-auth',
     keywords='python-social-auth,sso,josso',
-    version='0.1.0',
+    version='0.1.1',
     packages=['josso'],
+    package_dir={'josso': 'josso'},
+    package_data={'josso': ['wsdl/*.xml']},
     install_requires=['suds-jurko', 'six'],
     url='https://github.com/consbio/python-josso-auth',
     license='BSD'
